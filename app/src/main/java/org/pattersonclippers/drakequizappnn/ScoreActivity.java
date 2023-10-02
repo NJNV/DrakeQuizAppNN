8e550aa137cf6e607966af2186058dd8093d9701
Naglieri <1176160@bcpss.org>
2023-10-02 14:14:05 -0400

    Quiz App from the end of unit 1

---
+++ app/src/main/java/org/pattersonclippers/drakequizappnn/ScoreActivity.java
@@ -0,0 +1,31 @@
+package org.pattersonclippers.drakequizappnn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+
+    Intent incomingIntent;
+    TextView scoreTV;
+    int score;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        scoreTV = (TextView) findViewById(R.id.scoreTV);
+        incomingIntent = getIntent();
+        score = incomingIntent.getIntExtra("score",0);
+        scoreTV.setText("" + score);
+
+
+
+
+    }
+}
