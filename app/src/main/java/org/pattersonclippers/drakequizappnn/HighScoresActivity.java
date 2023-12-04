e039806aa27bb7934d5b53fc023b42f749490395
NJNV <Naglierinick57@gmail.com>
2023-12-04 14:49:02 -0500

    HighScore
    I made some texts views for highscore

--- app/src/main/java/org/pattersonclippers/drakequizappnn/HighScoresActivity.java
+++ app/src/main/java/org/pattersonclippers/drakequizappnn/HighScoresActivity.java
@@ -3,12 +3,20 @@ package org.pattersonclippers.drakequizappnn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HighScoresActivity extends AppCompatActivity {
+
+    TextView NameTV,HighScoreTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores);
+
+        NameTV = (TextView) findViewById(R.id.NameTV);
+        HighScoreTV = (TextView) findViewById(R.id.HighScoreTV);
+
     }
 }
